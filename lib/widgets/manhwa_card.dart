6616f5dea14ca980a6589d59369512510638c9f6
MahiFaiyaz <faiyaz1997@gmail.com>
2025-04-14 02:27:19 -0400

    Updated manhwa card to accept manhwa object

--- lib/widgets/manhwa_card.dart
+++ lib/widgets/manhwa_card.dart
@@ -1,50 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:manhwa_tracker/widgets/manhwa_detail_popup.dart';
 
 class ManhwaCard extends StatelessWidget {
-  final String title;
-  final String imageUrl;
+  final Map<String, dynamic> manhwa;
 
-  const ManhwaCard({super.key, required this.title, required this.imageUrl});
+  const ManhwaCard({super.key, required this.manhwa});
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(12),
-      child: Stack(
-        children: [
-          AspectRatio(
-            aspectRatio: 2 / 3,
-            child: Image.network(
-              imageUrl,
-              fit: BoxFit.cover,
-              width: double.infinity,
-              errorBuilder:
-                  (context, error, stackTrace) => Container(
-                    color: Colors.grey[300],
-                    child: const Icon(Icons.broken_image),
-                  ),
-            ),
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-              color: Colors.black.withOpacity(0.6), // subtle dark overlay
-              child: Text(
-                title,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 13,
-                  fontWeight: FontWeight.w600,
-                ),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
+    final title = manhwa["name"] ?? "";
+    final imageUrl = manhwa["image_url"] ?? "";
+
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          isScrollControlled: true,
+          showDragHandle: true,
+          builder: (_) => ManhwaDetailPopup(manhwa: manhwa),
+        );
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Stack(
+          children: [
+            AspectRatio(
+              aspectRatio: 2 / 3,
+              child: Image.network(
+                imageUrl,
+                fit: BoxFit.cover,
+                width: double.infinity,
+                errorBuilder:
+                    (context, error, stackTrace) => Container(
+                      color: Colors.grey[300],
+                      child: const Icon(Icons.broken_image),
+                    ),
               ),
             ),
-          ),
-        ],
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+                color: Colors.black.withOpacity(0.6), // subtle dark overlay
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 13,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
