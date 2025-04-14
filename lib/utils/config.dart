3e5970b93573f4d5049ca0419bfe298f99c601fd
MahiFaiyaz <faiyaz1997@gmail.com>
2025-04-14 18:39:25 -0400

    Updated api url

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,1 +1,2 @@
-const String apiBaseUrl = 'https://manhwa-tracker-backend.onrender.com/'; // or local tunnel when testing
+const String apiBaseUrl =
+    'https://manhwa-tracker-backend.onrender.com'; // or local tunnel when testing
