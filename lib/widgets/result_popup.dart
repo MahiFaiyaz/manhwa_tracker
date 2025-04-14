5592876e4c425cbd135d662e1e6dda329fecf6d5
MahiFaiyaz <faiyaz1997@gmail.com>
2025-04-14 01:30:23 -0400

    Added fetch manhwas to result popup

--- lib/widgets/result_popup.dart
+++ lib/widgets/result_popup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'manhwa_card.dart'; // adjust import path
+import 'package:manhwa_tracker/services/api_services.dart';
+import 'manhwa_card.dart';
 
 class ResultPopup extends StatelessWidget {
   const ResultPopup({super.key});
@@ -8,33 +9,41 @@ class ResultPopup extends StatelessWidget {
   Widget build(BuildContext context) {
     final height = MediaQuery.sizeOf(context).height;
 
-    final manhwaList = List.generate(20, (index) {
-      return {
-        "name": "Manhwa $index",
-        "image_url": "https://cdn.myanimelist.net/images/manga/2/129069.jpg",
-      };
-    });
-
     return SizedBox(
       height: height * 0.83,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-        child: GridView.builder(
-          itemCount: manhwaList.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            mainAxisSpacing: 12,
-            crossAxisSpacing: 12,
-            childAspectRatio: 2 / 3,
-          ),
-          itemBuilder: (context, index) {
-            final manhwa = manhwaList[index];
-            return ManhwaCard(
-              title: manhwa["name"] ?? "",
-              imageUrl: manhwa["image_url"] ?? "",
-            );
-          },
-        ),
+      child: FutureBuilder<List<Map<String, dynamic>>>(
+        future: fetchManhwas(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text("No results found."));
+          }
+
+          final manhwas = snapshot.data!;
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            child: GridView.builder(
+              itemCount: manhwas.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 12,
+                crossAxisSpacing: 12,
+                childAspectRatio: 2 / 3,
+              ),
+              itemBuilder: (context, index) {
+                final manhwa = manhwas[index]["manhwa"];
+                return ManhwaCard(
+                  title: manhwa["name"] ?? "",
+                  imageUrl: manhwa["image_url"] ?? "",
+                );
+              },
+            ),
+          );
+        },
       ),
     );
   }
