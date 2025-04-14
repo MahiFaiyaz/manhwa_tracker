8c73584ba7dc947057b69ec0c646b3afe1a06316
MahiFaiyaz <faiyaz1997@gmail.com>
2025-04-14 18:57:44 -0400

    Updated filter model

--- lib/models/manhwa_filter.dart
+++ lib/models/manhwa_filter.dart
@@ -1,32 +1,52 @@
 class ManhwaFilter {
-  final List<String> genres;
-  final List<String> categories;
-  final List<String> status;
-  final List<String> ratings;
-  final int minChapters;
-  final int maxChapters;
-  final int minYearReleased;
-  final int maxYearReleased;
+  final List<String>? genres;
+  final List<String>? categories;
+  final List<String>? status;
+  final List<String>? ratings;
+  final int? minChapters;
+  final int? maxChapters;
+  final int? minYearReleased;
+  final int? maxYearReleased;
 
   ManhwaFilter({
-    this.genres = const [],
-    this.categories = const [],
-    this.status = const [],
-    this.ratings = const [],
-    this.minChapters = 0,
-    this.maxChapters = 0,
-    this.minYearReleased = 0,
-    this.maxYearReleased = 0,
+    this.genres,
+    this.categories,
+    this.status,
+    this.ratings,
+    this.minChapters,
+    this.maxChapters,
+    this.minYearReleased,
+    this.maxYearReleased,
   });
 
-  Map<String, dynamic> toJson() => {
-    "genres": genres,
-    "categories": categories,
-    "status": status,
-    "ratings": ratings,
-    "min_chapters": minChapters,
-    "max_chapters": maxChapters,
-    "min_year_released": minYearReleased,
-    "max_year_released": maxYearReleased,
-  };
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> json = {};
+
+    if (genres != null && genres!.isNotEmpty) {
+      json['genres'] = genres;
+    }
+    if (categories != null && categories!.isNotEmpty) {
+      json['categories'] = categories;
+    }
+    if (status != null && status!.isNotEmpty) {
+      json['status'] = status;
+    }
+    if (ratings != null && ratings!.isNotEmpty) {
+      json['ratings'] = ratings;
+    }
+    if (minChapters != null && minChapters! > 0) {
+      json['min_chapters'] = minChapters;
+    }
+    if (maxChapters != null && maxChapters! > 0) {
+      json['max_chapters'] = maxChapters;
+    }
+    if (minYearReleased != null && minYearReleased! > 0) {
+      json['min_year_released'] = minYearReleased;
+    }
+    if (maxYearReleased != null && maxYearReleased! > 0) {
+      json['max_year_released'] = maxYearReleased;
+    }
+
+    return json;
+  }
 }
