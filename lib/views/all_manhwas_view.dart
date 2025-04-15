c2c0630e6ab9ee5a9781b343737147f419d4d218
MahiFaiyaz <faiyaz1997@gmail.com>
2025-04-15 11:09:57 -0400

    Added version number

--- lib/views/all_manhwas_view.dart
+++ lib/views/all_manhwas_view.dart
@@ -48,7 +48,7 @@ class _AllManhwasViewState extends State<AllManhwasView> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget.title + ' Version 0.1'),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
