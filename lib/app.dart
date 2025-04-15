1c0ae1808c8a56d1f663fa0e3f62571b3b475b93
MahiFaiyaz <faiyaz1997@gmail.com>
2025-04-15 10:49:41 -0400

    Added constraints

--- lib/app.dart
+++ lib/app.dart
@@ -4,13 +4,28 @@ import 'views/root_view.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Manhwa Tracker',
-      theme: ThemeData.dark(),
-      home: const RootView(),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        // Optional: limit max width for desktop users
+        final double maxAllowedWidth = 600; // or 800, tweak as you like
+        final double width =
+            constraints.maxWidth > maxAllowedWidth
+                ? maxAllowedWidth
+                : constraints.maxWidth;
+
+        return Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minWidth: width, maxWidth: width),
+            child: MaterialApp(
+              title: 'Manhwa Tracker',
+              theme: ThemeData.dark(),
+              home: const RootView(),
+            ),
+          ),
+        );
+      },
     );
   }
 }
